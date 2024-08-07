91466b72a2004316aa85fee569904ad231b66a72
Gesa Stupperich <gesa.stupperich@gmail.com>
2024-08-07 20:49:51 +0100

    Attackers and defenders are intersecting sets of characters

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,14 +1,16 @@
-use rand::seq::SliceRandom;
+use rand::Rng;
 use rand::thread_rng;
+use std::fmt::Debug;
 
 trait CanHit {
     fn hit(&self, target: &mut dyn CanTakeHit);
 }
 
-trait CanTakeHit {
+trait CanTakeHit: Debug {
     fn take_hit(&mut self, damage: i32);
 }
 
+#[derive(Debug)]
 struct Goblin {
     health: i32,
     damage: i32,
@@ -22,6 +24,10 @@ impl Goblin {
 
 impl CanHit for Goblin {
     fn hit(&self, target: &mut dyn CanTakeHit) {
+        if self.health == 0 {
+            println!("Goblin is already game over and cannot hit anything");
+            return;
+        }
         println!("Goblin hits");
         target.take_hit(self.damage);
     }
@@ -29,14 +35,19 @@ impl CanHit for Goblin {
 
 impl CanTakeHit for Goblin {
     fn take_hit(&mut self, damage: i32) {
-        println!("Goblin got hit");
+        if self.health == 0 {
+            println!("Goblin is already game over and cannot take any more hits");
+            return;
+        }
         self.health = std::cmp::max(0, self.health - damage);
+        println!("Goblin got hit, health is now {}", self.health);
         if self.health == 0 {
             println!("Goblin game over");
         }
     }
 }
 
+#[derive(Debug)]
 struct Orc {
     health: i32,
     damage: i32,
@@ -50,6 +61,10 @@ impl Orc {
 
 impl CanHit for Orc {
     fn hit(&self, target: &mut dyn CanTakeHit) {
+        if self.health == 0 {
+            println!("Orc is game over and cannot hit anything");
+            return;
+        }
         println!("Orc hits");
         target.take_hit(self.damage);
     }
@@ -57,14 +72,18 @@ impl CanHit for Orc {
 
 impl CanTakeHit for Orc {
     fn take_hit(&mut self, damage: i32) {
-        println!("Orc got hit");
+        if self.health == 0 {
+            println!("Orc is already game over and cannot take any more hits");
+        }
         self.health = std::cmp::max(0, self.health - damage);
+        println!("Orc got hit, health is now {}", self.health);
         if self.health == 0 {
             println!("Orc game over");
         }
     }
 }
 
+#[derive(Debug)]
 struct Building {
     health: i32,
 }
@@ -77,8 +96,11 @@ impl Building {
 
 impl CanTakeHit for Building {
     fn take_hit(&mut self, damage: i32) {
-        println!("Building got hit");
+        if self.health == 0 {
+            println!("Building has already been destroyed");
+        }
         self.health = std::cmp::max(0, self.health - damage);
+        println!("Building got hit, health is now {}", self.health);
         if self.health == 0 {
             println!("Building destroyed");
         }
@@ -86,24 +108,34 @@ impl CanTakeHit for Building {
 }
 
 fn main() {
-    let attackers: Vec<Box<dyn CanHit>> = vec![
-        Box::new(Goblin::new()),
-        Box::new(Orc::new()),
-    ];
-    let mut defenders: Vec<Box<dyn CanTakeHit>> = vec![
-        Box::new(Goblin::new()),
-        Box::new(Orc::new()),
-        Box::new(Building::new()),
-    ];
+    let mut goblin = Goblin::new();
+    let mut orc = Orc::new();
+    let mut building = Building::new();
 
     println!("Fight commences");
 
     for i in 0..10 {
         println!("\nRound {}", i);
         let mut rng = thread_rng();
-        let defender = defenders.choose_mut(&mut rng).unwrap();
-        let attacker = attackers.choose(&mut rng).unwrap();
-        attacker.hit(defender.as_mut());
+        let attacker = rng.gen_range(0..2);
+        let defender = rng.gen_range(0..2);
+        match attacker {
+            0 => {
+                match defender {
+                    0 => goblin.hit(&mut orc),
+                    1 => goblin.hit(&mut building),
+                    _ => (),
+                }
+            }
+            1 => {
+                match defender {
+                    0 => orc.hit(&mut goblin),
+                    1 => orc.hit(&mut building),
+                    _ => (),
+                }
+            }
+            _ => (),
+        }
     }
 
     println!("\nFight ends");
@@ -138,7 +170,7 @@ mod tests {
     }
 
     #[test]
-    fn test_health_never_below_zero() {
+    fn test_health_never_drops_below_zero() {
         let orc = Orc::new();
         let mut goblin = Goblin::new();
         orc.hit(&mut goblin);
@@ -147,5 +179,20 @@ mod tests {
         orc.hit(&mut goblin);
         orc.hit(&mut goblin);
         assert_eq!(goblin.health, 0);
+    }
+
+    #[test]
+    fn test_dead_goblin_cannot_damage_building() {
+        let orc = Orc::new();
+        let mut goblin = Goblin::new();
+        let mut building = Building::new();
+        orc.hit(&mut goblin);
+        orc.hit(&mut goblin);
+        orc.hit(&mut goblin);
+        orc.hit(&mut goblin);
+        orc.hit(&mut goblin);
+        assert_eq!(goblin.health, 0);
+        goblin.hit(&mut building);
+        assert_eq!(building.health, 15);
     }
 }
